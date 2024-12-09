8fb1f4bf9bc69313d7c989a6566e09b2b3048dc7
MikeBoguszewski <133729602+MikeBoguszewski@users.noreply.github.com>
2024-12-08 18:48:07 -0600

    Shorten text on homepage.

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -19,16 +19,7 @@ export default function HomePage() {
               possibilities with our extensive collection of items, from fashion
               and electronics to home decor and more. Whether you're looking for
               the latest trends, tech gadgets, or unique gifts, we've got you
-              covered. Why shop with us? Variety: Explore a curated selection of
-              products, handpicked to meet your every need. Quality: We pride
-              ourselves on providing only the highest quality items to ensure
-              your satisfaction. Affordability: Enjoy competitive prices and
-              special discounts on a range of products. Convenience: Shop from
-              the comfort of your home, and we'll deliver your purchases right
-              to your doorstep. Customer Support: Our dedicated customer support
-              team is here to assist you with any queries or concerns. Join our
-              growing community of happy shoppers and experience the convenience
-              of online shopping at its best. Start browsing our store today,
+              covered. Start browsing our store today,
               and let us help you find exactly what you're looking for. Thank
               you for choosing Fake Store. We look forward to serving you!
             </span>
